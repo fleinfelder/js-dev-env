90c8c1c76d9d32883a63b86ef29987a2b49d2469
Florian Leinfelder <leinfelder@me.com>
2017-04-27 23:40:36 +0200

    Updated tests to follow new JSDOM 10.0 approach

--- src/index.test.js
+++ src/index.test.js
@@ -1,17 +1,12 @@
-import jsdom from 'jsdom';
 import fs from 'fs';
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
 
 describe('index.html', () => {
   test('says hello', () => {
     const index = fs.readFileSync('./src/index.html', 'utf-8');
-    jsdom.env(index, function (err, window) {
-      if (err) {
-        console.log(err);
-      } else {
-        const h1 = window.document.getElementsByTagName('h1')[0];
-        expect(h1.innerHTML).toBe('Hello World!');
-      }
-      window.close();
-    });
+    const { document } = (new JSDOM(index)).window;
+    const h1 = document.querySelector('h1');
+    expect(h1.innerHTML).toBe('Hello World!');
   });
 });
